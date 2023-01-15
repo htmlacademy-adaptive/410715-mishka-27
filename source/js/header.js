1c586af1938bca4468d357e3f27da395f1bd935d
NikitaUrb <nikita160494@gmail.com>
2023-01-15 15:30:25 +0300

    Added retina image and fix

--- source/js/header.js
+++ source/js/header.js
@@ -1,10 +1,10 @@
-let buttonMenu = document.querySelector(".navigation-button");
+let buttonMenu = document.querySelector(".navigation--toggle");
 let dropMenu = document.querySelector(".navigation");
 
-buttonMenu.classList.remove('navigation-button--close');
+buttonMenu.classList.remove('navigation--toggle-closed');
 dropMenu.classList.remove('navigation--open');
 
 buttonMenu.onclick = () => {
-  buttonMenu.classList.toggle("navigation-button--close");
+  buttonMenu.classList.toggle("navigation--toggle-closed");
   dropMenu.classList.toggle("navigation--open");
 };
