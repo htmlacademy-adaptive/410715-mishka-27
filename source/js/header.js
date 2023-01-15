92c8b36b9be8f480d8146006b121751900971593
NikitaUrb <nikita160494@gmail.com>
2023-01-15 23:14:26 +0300

    Adde styleguide

--- source/js/header.js
+++ source/js/header.js
@@ -1,10 +1,10 @@
-let buttonMenu = document.querySelector(".navigation--toggle");
+let buttonMenu = document.querySelector(".navigation__toggle");
 let dropMenu = document.querySelector(".navigation");
 
-buttonMenu.classList.remove('navigation--toggle-closed');
+buttonMenu.classList.remove('navigation__toggle--closed');
 dropMenu.classList.remove('navigation--open');
 
 buttonMenu.onclick = () => {
-  buttonMenu.classList.toggle("navigation--toggle-closed");
+  buttonMenu.classList.toggle("navigation__toggle--closed");
   dropMenu.classList.toggle("navigation--open");
 };
