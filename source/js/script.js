14a947bf3ae4e24c703c4ec3486a5c1af029bd5c
NikitaUrb <nikita160494@gmail.com>
2023-01-21 12:50:07 +0300

    Fixed B3, B5, D16, D21, B28, D14

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,14 @@
-let buttonMenu = document.querySelector(".navigation__toggle");
+let buttonMenu = document.querySelector(".header__toggle");
 let dropMenu = document.querySelector(".navigation");
+let map = document.querySelector(".contacts__map-item");
+let mapImg  = document.querySelector(".contacts__image");
 
-buttonMenu.classList.remove('navigation__toggle--closed');
-dropMenu.classList.remove('navigation--open');
+mapImg.classList.add("visually-hidden");
+map.classList.remove("visually-hidden");
+buttonMenu.classList.remove("header__toggle--closed");
+dropMenu.classList.remove("navigation--open");
 
 buttonMenu.onclick = () => {
-  buttonMenu.classList.toggle("navigation__toggle--closed");
+  buttonMenu.classList.toggle("header__toggle--closed");
   dropMenu.classList.toggle("navigation--open");
 };
